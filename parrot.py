41f76ab1f4a154eb06a982b0673e2d898b41e1bb
simonesantarsiero04 <75539291+simonesantarsiero04@users.noreply.github.com>
2022-02-18 00:05:27 +0100

    Update parrot.py

--- parrot.py
+++ parrot.py
@@ -1,6 +1,6 @@
 class Parrot():
   
-  def __init__(self, model_tag="prithivida/parrot_paraphraser_on_T5", use_gpu=False):
+  def __init__(self, model_tag="SEBIS/legal_t5_small_summ_it", use_gpu=False):
     from transformers import AutoTokenizer
     from transformers import AutoModelForSeq2SeqLM
     import pandas as pd
